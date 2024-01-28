d014cc649b0f1105dc5be3e89180e3dc14549368
GSNadarajan <nadarajangs31@gmail.com>
2024-01-28 13:01:13 +0530

    feat : next-js updated structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Roboto } from "next/font/google";
-import "./globals.css"; 
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const roboto = Roboto({ subsets: ['latin'],weight : ['400' ,'500', '700' ]})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,11 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
-        <main className="max-w-4xl mx-auto border">
-         {children}
-        </main>     
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
