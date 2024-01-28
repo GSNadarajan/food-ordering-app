ba534076f6aac361fb2fe3c6021456381a3492db
GSNadarajan <nadarajangs31@gmail.com>
2024-01-28 21:43:30 +0530

    feat : updated hero section

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -4,13 +4,18 @@ import Right from "../icons/Right";
 export default function Hero() {
   return (
     <>
-      <section className="grid grid-cols-2">
-        <div className="md:py-16">
+      <section className="hero">
+        <div className="py-12">
           <h1 className="text-4xl font-semibold">
-            Everything is better with a Pizza
+            Everything <br />
+             is better <br /> with a&nbsp;
+             <span className="text-primary">
+             Pizza
+
+             </span>
           </h1>
-          <p className="my-4 text-gray-500">
-            Pizza is the missing piece that makes everyday complete, a simple
+          <p className="my-4 text-gray-500 text-sm ">
+            Pizza is the missing piece that makes everyday complete, <br /> a simple
             yet delicious joy in life
           </p>
           <div className="flex gap-4 text-sm">
